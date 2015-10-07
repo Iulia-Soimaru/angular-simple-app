257316ec868ce0282834ad4562d31d56df5e6867
Iulia Soimaru <iuliashoimaru@gmail.com>
2015-10-06 19:29:46 -0700

    add more about product

--- app.js
+++ app.js
@@ -4,6 +4,16 @@
     this.products = gems
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  })
+
   var gems = [
     {
       name: "Dodecahedron",
@@ -11,6 +21,23 @@
       description: 'wt wt wt wt',
       canPurchase: true,
       soldOut: false,
+      reviews: [
+        {
+          stars: 5,
+          body: "I love it",
+          author: "iulia@gmail.com",
+        },
+        {
+          stars: 4,
+          body: "I love it",
+          author: "tom@gmail.com",
+        },
+        {
+          stars: 5,
+          body: "I love it",
+          author: "alex@gmail.com",
+        }
+      ],
     },
     {
       name: "Something",
@@ -18,6 +45,23 @@
       description: 'ert eetg ssgwf',
       canPurchase: true,
       soldOut: false,
+      reviews: [
+        {
+          stars: 5,
+          body: "I love it",
+          author: "iulia@gmail.com",
+        },
+        {
+          stars: 4,
+          body: "I love it",
+          author: "tom@gmail.com",
+        },
+        {
+          stars: 5,
+          body: "I love it",
+          author: "alex@gmail.com",
+        }
+      ],
     },
     {
       name: "Something",
@@ -25,21 +69,24 @@
       description: 'ert eetg ssgwf',
       canPurchase: true,
       soldOut: false,
+      reviews: [
+        {
+          stars: 5,
+          body: "I love it",
+          author: "iulia@gmail.com",
+        },
+        {
+          stars: 4,
+          body: "I love it",
+          author: "tom@gmail.com",
+        },
+        {
+          stars: 5,
+          body: "I love it",
+          author: "alex@gmail.com",
+        }
+      ],
     },
-    {
-      name: "Something",
-      price: 6.95,
-      description: 'ert eetg ssgwf',
-      canPurchase: true,
-      soldOut: false,
-    },
-    {
-      name: "Something",
-      price: 3.95,
-      description: 'ert eetg ssgwf',
-      canPurchase: true,
-      soldOut: false,
-    }
   ]
 
 })();
